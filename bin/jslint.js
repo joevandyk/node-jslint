ae4156eb837307e7872a0c8a72a96e7ecf5ca2ce
Joe Van Dyk <joe@fixieconsulting.com>
2010-09-27 07:45:09 -0700

    Added Joe's JS Preferences

--- bin/jslint.js
+++ bin/jslint.js
@@ -30,8 +30,22 @@
     input = input.replace(/^\#\!.*/, "");
 
     success = JSLINT(input, {
+        passfail: true,
+        white:    true,
+        browser:  true,
+        onevar:   true,
+        undef:    true,
+        nomen:    true,
+        eqeqeq:   true,
+        plusplus: true,
+        bitwise:  true,
+        regexp:   true,
+        newcap:   true,
+        immed:    true,
+        maxlen:   80,
+        indent:   2,
         predef:   [ // CommonJS
-                    "exports", 
+                    "exports",
                     // YUI
                     "YUI",
                     "YAHOO",
